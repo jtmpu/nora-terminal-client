f1d649d575e089c526f34f8d75bac5ba70c88604
somethign <yeah.right@legitmail.tld>
2017-01-22 22:15:05 +0100

    Hotfix bla bla bla

--- client.py
+++ client.py
@@ -10,6 +10,7 @@ import norac.connection
 DEFAULT_CONFIG_NAME="norac_default.cfg"
 OVERRIDE_CONFIG_NAME="norac.cfg"
 
+# Parse Configurations
 config = ConfigParser.ConfigParser()
 with open(DEFAULT_CONFIG_NAME, "r") as f:
     config.readfp(open(DEFAULT_CONFIG_NAME))
@@ -27,12 +28,22 @@ if (config.get("security", "cafile") == default.get("security", "cafile")
     and config.get("security", "key") == default.get("security", "key")):
     # Default certificates are used in the application
     print("WARNING: EXAMPLE CERTIFICATES USED.")
+    print("")
 if not config.getboolean("security", "verify"):
     print("WARNING: NOT VERIFYING SERVER'S AUTHENTICITY")
+    print("")
 
+# Check for arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--command", type=str, help="Get information about all or a command on the server. (Too see available commands, use the 'all' command.)")
+args = parser.parse_args()
 
 connection = norac.connection.Connection()
 connection.init(config)
 
-response = connection.send_request({ "command" : "help", "meta" : [], "data" : "add_note" })
-print(response["data"])
+if args.command:
+    data = ""
+    if args.command != "all":
+        data = args.command
+    response = connection.send_request({ "command" : "commands", "meta" : [], "data" : data })
+    print(response["data"])
